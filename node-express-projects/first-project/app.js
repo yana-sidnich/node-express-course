17de61875122a67421ee6eb5c73c6065fa6cbf53
yana-sidanych <yana.sid150@gmail.com>
2021-10-28 02:07:00 +0300

    clear up mess

--- node-express-projects/first-project/app.js
+++ node-express-projects/first-project/app.js
@@ -1,1 +1,24 @@
-console.log('Express Tutorial')
+// console.log("Express Tutorial");
+
+const https = require("http");
+const server = https.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>hello page</h1>");
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  } else if (url === "/contact") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>contact page</h1>");
+    res.end();
+  } else {
+    res.writeHead(400, { "conten-type": "text/html" });
+    res.write("<h1>not found page</h1>");
+  }
+});
+
+server.listen(5000);
--- node-express-projects/test.js
+++
@@ -1,7 +0,0 @@
-const os = require("os");
-
-const user = os.userInfo();
-console.log(user);
-
-const time = os.uptime();
-console.log(time);
